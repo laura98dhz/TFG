90aba5ca721d3ee9d063afc32d874c6bbc739b70
laura98dhz <laura98dhz@gmail.com>
2022-03-16 19:49:45 +0100

    pag prin toda hecha

---
+++ golden-houses/src/App.js
@@ -0,0 +1,17 @@
+import './components/scss/main.scss';
+
+import Header from './components/Header/Header.js';
+import MainPrincipal from './components/Main/MainPrincipal';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <MainPrincipal/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
